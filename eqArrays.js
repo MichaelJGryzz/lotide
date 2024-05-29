317a121f363bfeae3b44cda1698ec12dae9e32ae
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-05-29 06:05:16 -0400

    Add eqArrays function to compare two arrays for strict equality

---
+++ eqArrays.js
@@ -0,0 +1,36 @@
+// Copied function from assertEqual.js file:
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// eqArrays function takes in two arrays and returns true or false based on a perfect match.
+const eqArrays = function(array1, array2) {
+  // Checks if length are the same; will not enter for loop if they aren't the same
+  if (array1.length !== array2.length) {
+    return false;
+  // Check if each element is exactly equal
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  // ALL elements are equal, return true
+  return true;
+};
+
+// Test Cases:
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true); // => should PASS
+assertEqual(eqArrays(['1', '2', '3'], [1, '2', '3']), false); // => should PASS
+assertEqual(eqArrays([true, false, true], [true, false, true]), true); // => should PASS
+assertEqual(eqArrays([false, false, false], [true, false, false]), false); // => should PASS
+assertEqual(eqArrays([], []), true); // => should PASS
+assertEqual(eqArrays([1, 2], [1, 2, 3, 4]), false); // => should PASS
+assertEqual(eqArrays([null, undefined], [null, undefined]), true); // => should PASS
+assertEqual(eqArrays([1, 2, NaN], [1, 2, NaN]), false); // => should PASS because NaN !== NaN, therefore falsey
