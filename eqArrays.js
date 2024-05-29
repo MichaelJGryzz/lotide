27e8bc31875a2fba3216b6276a4bf9f29d20b974
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-05-29 06:26:18 -0400

    Move comment "Check if each element is exactly equal" inside the for loop instead of before it

--- eqArrays.js
+++ eqArrays.js
@@ -12,9 +12,9 @@ const eqArrays = function(array1, array2) {
   // Checks if length are the same; will not enter for loop if they aren't the same
   if (array1.length !== array2.length) {
     return false;
-  // Check if each element is exactly equal
   }
   for (let i = 0; i < array1.length; i++) {
+    // Check if each element is exactly equal
     if (array1[i] !== array2[i]) {
       return false;
     }
