92943a6065e4dea61be7aea4eb2627dcaffe2df2
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-05-29 20:19:08 -0400

    Add comments to middle function to help logic be more easily understood

--- middle.js
+++ middle.js
@@ -25,14 +25,17 @@ const assertArraysEqual = function(actual, expected) {
 // ACTUAL FUNCTION
 const middle = function(array) {
   const length = array.length;
+  // Return an empty array if given array has less than 3 elements
   if (length <= 2) {
     return [];
   }
   // Calculate the middle index using floor division
   const middleIndex = Math.floor(length / 2);
+  // If the array length is even, return the two middle elements
   if (length % 2 === 0) {
     return [array[middleIndex - 1], array[middleIndex]];
   } else {
+    // If the array length is odd, return the single middle element
     return [array[middleIndex]];
   }
 };
