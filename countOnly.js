6dd6eb070547a815fdf55c7ac2fe1d40b2a20b8d
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-04 19:12:47 -0400

    Add and implement countOnly function to filter through a provided array of items and count only the specified items in itemsToCount object

---
+++ countOnly.js
@@ -0,0 +1,58 @@
+// assertEqual function copied from previous task
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  // Initialize empty object and return
+  const results = {};
+
+  for (const item of allItems) { // Loops over allItems
+    console.log(item); // Print each item to console for debugging purposes
+    if (itemsToCount[item]) { // Check if the item exists and it should be counted
+      if (results[item]) {
+        results[item] += 1; // Add to the count if item is already in results
+      } else {
+        results[item] = 1; // Start the count if item si not in results
+      }
+    }
+  }
+
+  return results;
+};
+
+// Provided array of names
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+// TEST CODE
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+console.log('Result:', result1); // Print output of countOnly for debugging
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+assertEqual(result1["Michael"], undefined); // String does not exist in input array
+assertEqual(result1["Diana"], undefined); // String does not exist in input array
