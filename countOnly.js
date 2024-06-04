05d113b973e071e06197da82fd5572ab3b292f4a
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-04 19:17:41 -0400

    Add "console.log" to print actual output of countOnly for debugging purposes

--- countOnly.js
+++ countOnly.js
@@ -50,9 +50,9 @@ const result1 = countOnly(firstNames, {
 
 console.log('Result:', result1); // Print output of countOnly for debugging
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
-assertEqual(result1["Michael"], undefined); // String does not exist in input array
-assertEqual(result1["Diana"], undefined); // String does not exist in input array
+assertEqual(result1["Jason"], 1); // Pass
+assertEqual(result1["Karima"], undefined); // Pass
+assertEqual(result1["Fang"], 2); // Pass
+assertEqual(result1["Agouhanna"], undefined); // Pass
+assertEqual(result1["Michael"], undefined); // Pass because string does not exist in input array
+assertEqual(result1["Diana"], undefined); // Pass because string does not exist in input array
