409fcb7ecc63aa7c9f3ae6a799cdd4166382ef87
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-05 02:01:39 -0400

    Add equalObjects helper function to compare two objects for strict equality

--- countLetters.js
+++ countLetters.js
@@ -22,14 +22,17 @@ const countLetters = function(sentence) {
   return letterCount;
 };
 
+// Helper function to check of two objects are equal by comparing both keys and values
 const equalObjects = function(obj1, obj2) {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
 
+  // Check if both objects have same number of keys
   if (keys1.length !== keys2.length) {
     return false;
   }
 
+  // Check if values for each key are the same in both objects
   for (const key of keys1) {
     if (obj1[key] !== obj2[key]) {
       return false;
@@ -40,6 +43,7 @@ const equalObjects = function(obj1, obj2) {
 };
 
 // Test Code
+// Use both assertEqual and countLetters with expected code to compare results
 assertEqual(equalObjects(countLetters('LHL'), { L: 2, H: 1 }), true);
 assertEqual(equalObjects(countLetters('lighthouse in the house'), { l: 1, i: 2, g: 1, h:4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }), true);
 assertEqual(equalObjects(countLetters('Michael'), { M: 1, i: 1, c: 1, h: 1, a: 1, e: 1, l: 1 }), true);
