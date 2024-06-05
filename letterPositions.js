b5d0e4db6ef5428677927b16403468e230a83317
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-05 02:32:33 -0400

    Implement letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,6 @@
+// letterPositions function to return all indices in the string where each character is found
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  return results;
+};
