2b30cd4b3cbab851fe6110a80c0e2190000b4a7f
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-05 15:45:49 -0400

    Copy previously implemented "eqObjects" function into file

---
+++ assertObjectsEqual.js
@@ -0,0 +1,22 @@
+// Copied previously implemented eqObjects function
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  
+  // Check if both objects have same number of keys, if not return false
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key],object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
