98e94195499683e66804c0098ab00db289d4c8f3
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-05 14:57:46 -0400

    Copy previously implemented "eqArrays" function into file

--- eqObjects.js
+++ eqObjects.js
@@ -7,6 +7,20 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// Copied previously implemeted eqArrays function
+// This function checks if two arrays are equal
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
