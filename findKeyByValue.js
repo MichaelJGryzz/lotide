8ed52aeda8daf11593a3bf07736c11847733f268
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-05 05:07:29 -0400

    Copy previously implemented "assertEqual" function into file

---
+++ findKeyByValue.js
@@ -0,0 +1,9 @@
+// assertEqual function copied from previous task
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
