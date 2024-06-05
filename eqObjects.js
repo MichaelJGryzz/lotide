cb084b232cff3ed19e1a6bb124ca9f4993222a7c
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-05 15:38:16 -0400

    Delete unnecessary variables for current key in "object1" and "object2" to simplify code

--- eqObjects.js
+++ eqObjects.js
@@ -33,14 +33,12 @@ const eqObjects = function(object1, object2) {
   }
 
   for (const key of keys1) {
-    const value1 = object1[key];
-    const value2 = object2[key];
 
-    if (Array.isArray(value1) && Array.isArray(value2)) {
-      if (!eqArrays(value1,value2)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key],object2[key])) {
         return false;
       }
-    } else if (value1 !== value2) {
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
