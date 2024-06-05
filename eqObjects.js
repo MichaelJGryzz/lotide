95c2f38460cfed56ac13e7af17eeef05c3c64b5c
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-05 06:46:15 -0400

    Implement "eqObjects" function and add test cases utlizing "assertEqual" function

--- eqObjects.js
+++ eqObjects.js
@@ -9,4 +9,26 @@ const assertEqual = function(actual, expected) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {};
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  
+  // Check if both objects have same number of keys
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+};
+
+
+// Test Code
+// Use both assertEqual and equalObjects to compare countLetters function output with expected output
+// Test Case # 1
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+eqObjects(shirtObject, anotherShirtObject); // => true
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+
+// Test Case # 2
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject, longSleeveShirtObject); // => false
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
