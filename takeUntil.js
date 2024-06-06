2a25878fe89826f4f6b826b7fa943399b33e1332
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-05 22:15:37 -0400

    Copy previously implemented "assertArraysEqual" and "eqArrays" functions to file for assertion testing purposes

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,28 @@
+// Copied previously implemented eqArrays and assertArraysEqual functions:
+// eqArrays function checks if two arrays are equal
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// assertArraysEqual function takes in two arrays and console.log an appropriate message to the console
+// This function asserts if two arrays are equal and logs the result
+const assertArraysEqual = function(actual, expected) {
+  const isEqual = eqArrays(actual, expected);
+  if (isEqual) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
 // takeUntil funtion takes in an array to work with and the callback function
 // It returns a slice of the array with elements taken from the beginning
 const takeUntil = function(array, callback) {
