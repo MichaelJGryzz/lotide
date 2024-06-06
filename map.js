1e04444df45fa65f7200d1708bde9a55b6c436ae
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-05 20:12:35 -0400

    Add code copied from "Compass" to call nonexistent "map" function to cause first error

--- map.js
+++ map.js
@@ -1,2 +1,6 @@
 // Collection of words copied from Compass "Implement Map" Assignment
 const words = ["ground", "control", "to", "major", "tom"];
+
+// Add code copied from Compass to call nonexistent "map" function to cause first error
+const results1 = map(words, (word) => word[0]);
+console.log(results1);
