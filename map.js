22a7197006c2a3ec3a2b5eea27a6050f2a2a07d1
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-05 20:16:24 -0400

    Define empty "map" function

--- map.js
+++ map.js
@@ -1,6 +1,11 @@
 // Collection of words copied from Compass "Implement Map" Assignment
 const words = ["ground", "control", "to", "major", "tom"];
 
+// Define empty map function
+const map = function () {
+  // empty for now :)
+};
+
 // Add code copied from Compass to call nonexistent "map" function to cause first error
 const results1 = map(words, (word) => word[0]);
 console.log(results1);
