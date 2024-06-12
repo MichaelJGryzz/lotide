50f1fefa6dabf6b84e26742bb1a0419ad3b4d438
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-12 12:23:53 -0400

    Delete all the old commented out test code.

--- test/middleTest.js
+++ test/middleTest.js
@@ -26,20 +26,3 @@ describe("#middle", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
   });
 });
-
-/*
-// assertArraysEqual function asserts if two arrays are equal and logs the result
-// Require function assertArraysEqual
-const assertArraysEqual = require("../assertArraysEqual");
-
-// Require function middle
-const middle = require("../middle");
-
-
-// TEST CASES
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);*/
