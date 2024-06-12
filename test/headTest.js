bc499044481313a6c542305139aa30ad7f00f86c
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-12 00:00:17 -0400

    Add two "it" statements in the "describe" callback function which are the new assertion tests.

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,26 @@
+// Require Chai
+const assert = require('chai').assert;
+
+// Require head function
+const head   = require('../head');
+
+
+// Mocha and Chai based Test Code:
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
+
+
 // Require function assertEqual
-const assertEqual = require("../assertEqual");
+//const assertEqual = require("../assertEqual");
 
 // Require function head
-const head = require("../head");
-
-
+//const head = require("../head");
 
 // Test Code for head function
 //assertEqual(head([5, 6, 7]), 5);
