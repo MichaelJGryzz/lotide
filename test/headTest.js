8f76e9eb9e74a1f04abfcf3712d908dfa22068e4
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-12 00:02:57 -0400

    Change how previous test code was commented out.

--- test/headTest.js
+++ test/headTest.js
@@ -16,14 +16,15 @@ describe("#head", () => {
 });
 
 
+/*
 // Require function assertEqual
-//const assertEqual = require("../assertEqual");
+const assertEqual = require("../assertEqual");
 
 // Require function head
-//const head = require("../head");
+const head = require("../head");
 
 // Test Code for head function
-//assertEqual(head([5, 6, 7]), 5);
-//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-//assertEqual(head([10]), 9);
-//assertEqual(head([10]), undefined);
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([10]), 9);
+assertEqual(head([10]), undefined);*/
