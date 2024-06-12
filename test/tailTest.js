7def5cb1821b0c1ba03167336af6d660ac588177
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-12 11:23:28 -0400

    Comment out all of the test code in preparation of adding "Mocha" and "Chai" based test code. Add four "it" statements in a "describe" callback function to serve as the new assertion tests.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,3 +1,35 @@
+// Require Chai
+const assert = require('chai').assert;
+
+// Require tail function
+const tail   = require('../tail');
+
+
+// Mocha and Chai based Test Code:
+describe("#tail", () => {
+  it("does not modify the original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const wordsCopy = words.slice(); // Create a copy of the array
+    tail(words);
+    assert.deepEqual(words, wordsCopy); // Assert that the original array is not modified
+  });
+  it("returns an array containing all but the first element when passed an array with more than one element", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const wordsTail = ["Lighthouse", "Labs"];
+    assert.deepEqual(tail(words), wordsTail);
+  });
+  it("returns an empty array when passed an array with only one element", () => {
+    const oneElementArray = ["Caramel"];
+    assert.deepEqual(tail(oneElementArray), []);
+  });
+  it("returns an empty array when passed an empty array", () => {
+    const emptyArray = [];
+    assert.deepEqual(tail(emptyArray), []);
+  });
+});
+
+
+/*
 // Require function assertEqual
 const assertEqual = require("../assertEqual");
 
@@ -18,4 +50,4 @@ assertEqual(oneElementResult.length, 0); // tail of a single element array shoul
 // Test Case # 3: Empty array
 const emptyArray = [];
 const emptyArrayResult = tail(emptyArray);
-assertEqual(emptyArrayResult.length, 0); // tail of an empty array should also be empty
+assertEqual(emptyArrayResult.length, 0); // tail of an empty array should also be empty*/
