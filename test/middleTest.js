9edefa1663f573036cf76b1dfb079551bdc22277
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-12 12:28:56 -0400

    Add 7th "it" statement assertion test in "describe" callback function to ensure the original array is not modified by the "middle" function.

--- test/middleTest.js
+++ test/middleTest.js
@@ -7,6 +7,12 @@ const middle   = require('../middle');
 
 // Mocha and Chai based Test Code:
 describe("#middle", () => {
+  it("make sure the original array was not altered by the middle function", () => {
+    const words = ["hello", "world", "lighthouse"];
+    const result = middle(words);
+    assert.deepEqual(result, ["world"]);
+    assert.strictEqual(words.length, 3);
+  });
   it("returns [] for [1]", () => {
     assert.deepEqual(middle([1]), []);
   });
