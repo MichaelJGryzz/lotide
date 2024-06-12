37c6c9ae93f519c20066a003b8ca16aad183ffd4
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-11 23:38:15 -0400

    Comment out all of the test code in preparation of adding "Mocha" and "Chai" based test code.

--- test/headTest.js
+++ test/headTest.js
@@ -4,8 +4,10 @@ const assertEqual = require("../assertEqual");
 // Require function head
 const head = require("../head");
 
+
+
 // Test Code for head function
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([10]), 9);
-assertEqual(head([10]), undefined);
+//assertEqual(head([5, 6, 7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(head([10]), 9);
+//assertEqual(head([10]), undefined);
