62bcf1de9dd50ebd0dcdd57fde6604cf5a12bd21
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-11 21:48:05 -0400

    Remove "assertEqual" function definition code because its not needed in this file anymore. Remove test code because it was moved to new "tailTest.js" test file. Add code at bottom of the file to export function "tail" from the module.

--- tail.js
+++ tail.js
@@ -1,12 +1,4 @@
-// Function from assertEqual.js file
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// Tail function definition
 const tail = function(array) {
   // Check if the array is empty or has only one element
   if (array.length <= 1) {
@@ -17,17 +9,5 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case: An array with only one element
-const oneElementArray = ["Caramel"];
-const oneElementResult = tail(oneElementArray);
-assertEqual(oneElementResult.length, 0); // tail of a single element array should be empty
-
-// Test Case: Empty Array4
-const emptyArray = [];
-const emptyArrayResult = tail(emptyArray);
-assertEqual(emptyArrayResult.length, 0); // tail of an empty array should also be empty
+// Code to export function
+module.exports = tail;
