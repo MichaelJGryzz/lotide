960f40856a04c2ba8deba50ce46d437df7369978
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-11 22:09:02 -0400

    Remove test code because it was moved to new "eqArraysTest.js" test file.

--- eqArrays.js
+++ eqArrays.js
@@ -17,15 +17,3 @@ const eqArrays = function(array1, array2) {
 
 // Code to export function
 module.exports = eqArrays;
-
-// Test Cases:
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true); // => should PASS
-assertEqual(eqArrays(['1', '2', '3'], [1, '2', '3']), false); // => should PASS
-assertEqual(eqArrays([true, false, true], [true, false, true]), true); // => should PASS
-assertEqual(eqArrays([false, false, false], [true, false, false]), false); // => should PASS
-assertEqual(eqArrays([], []), true); // => should PASS
-assertEqual(eqArrays([1, 2], [1, 2, 3, 4]), false); // => should PASS
-assertEqual(eqArrays([null, undefined], [null, undefined]), true); // => should PASS
-assertEqual(eqArrays([1, 2, NaN], [1, 2, NaN]), false); // => should PASS (because NaN !== NaN, therefore falsey)
