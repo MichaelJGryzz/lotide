299420dad073e0696104db197045076b762d8b85
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-11 21:13:58 -0400

    Remove test code because it was moved to new "headTest.js" test file. Add code at bottom of the file to export function "head" from the module.

--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-// Function from assertEqual.js file
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// head function defintion
 // Function to return the first element of an array
 const head = function(array) {
   if (array.length === 0) {
@@ -15,8 +7,5 @@ const head = function(array) {
   return array[0];
 };
 
-//Test cases
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([10]), 9);
-assertEqual(head([10]), undefined);
+// Code to export function
+module.exports = head;
