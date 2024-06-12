2c88c8760eab9660f077d97879fbf4e030abcf15
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-11 20:46:40 -0400

    Create test folder and inside of it create test file "assertEqualTest.js". Move test code from "assertEqual.js" to new test file to separate it. Add code at top of test file to require "assertEqual" function.

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+// Require function assertEqual.js
+const assertEqual = require("../assertEqual");
+
+// Test Code for assertEqual function
+assertEqual("Lighthouse", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 2);
+assertEqual(2, 2);
