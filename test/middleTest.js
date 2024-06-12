ac982610eec39c91cf03763d9e8343553782238f
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-12 12:16:53 -0400

    Delete all the old commented out test code.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,36 @@
+// Require Chai
+const assert = require('chai').assert;
+
+// Require tail function
+const middle   = require('../middle');
+
+
+// Mocha and Chai based Test Code:
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [4] for [1, 2, 3, 4, 5, 6, 7]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+  });
+  it("returns [5, 6] for [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+  });
+});
+
+/*
 // assertArraysEqual function asserts if two arrays are equal and logs the result
 // Require function assertArraysEqual
 const assertArraysEqual = require("../assertArraysEqual");
@@ -12,4 +45,4 @@ assertArraysEqual(middle([1, 2]), []);
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);*/
--- test/tailTest.js
+++ test/tailTest.js
@@ -27,27 +27,3 @@ describe("#tail", () => {
     assert.deepEqual(tail(emptyArray), []);
   });
 });
-
-
-/*
-// Require function assertEqual
-const assertEqual = require("../assertEqual");
-
-// Require function tail
-const tail = require("../tail");
-
-
-// Test Case # 1: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case # 2: An array with only one element
-const oneElementArray = ["Caramel"];
-const oneElementResult = tail(oneElementArray);
-assertEqual(oneElementResult.length, 0); // tail of a single element array should be empty
-
-// Test Case # 3: Empty array
-const emptyArray = [];
-const emptyArrayResult = tail(emptyArray);
-assertEqual(emptyArrayResult.length, 0); // tail of an empty array should also be empty*/
