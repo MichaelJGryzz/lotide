fcebbdfe1f2d07466e1c9a830780929a39aa3f5b
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-12 12:22:47 -0400

    Comment out all of the test code in preparation of adding "Mocha" and "Chai" based test code. Convert all six of the assertion tests to "it" statements in a "describe" callback function to serve as the new assertion tests.

--- test/middleTest.js
+++ test/middleTest.js
@@ -13,9 +13,6 @@ describe("#middle", () => {
   it("returns [] for [1, 2]", () => {
     assert.deepEqual(middle([1, 2]), []);
   });
-  it("returns [] for [1, 2]", () => {
-    assert.deepEqual(middle([1, 2]), []);
-  });
   it("returns [2] for [1, 2, 3]", () => {
     assert.deepEqual(middle([1, 2, 3]), [2]);
   });
