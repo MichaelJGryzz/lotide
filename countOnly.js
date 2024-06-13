8184c3780938b0baef761a09744f75b9684cf5f4
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 00:14:13 -0400

    Remove "assertEqual" function definition because it is not needed in this file anymore. Remove test code because it was moved to new "countOnlyTest.js" file.

--- countOnly.js
+++ countOnly.js
@@ -1,12 +1,4 @@
-// assertEqual function copied from previous task
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// countOnly function definition
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
@@ -28,33 +20,3 @@ const countOnly = function(allItems, itemsToCount) {
 
 // Code to export function
 module.exports = countOnly;
-
-// Provided array of names
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe",
-];
-
-// TEST CODE
-const result1 = countOnly(firstNames, {
-  Jason: true,
-  Karima: true,
-  Fang: true,
-  Agouhanna: false,
-});
-
-console.log('Result:', result1); // Print output of countOnly for debugging
-
-assertEqual(result1["Jason"], 1); // Pass; Jason appears once
-assertEqual(result1["Karima"], undefined); // Pass; Karima does not appear
-assertEqual(result1["Fang"], 2); // Pass; Fang appears twice
-assertEqual(result1["Agouhanna"], undefined); // Pass; Agouhanna is marked false, do not count
-assertEqual(result1["Michael"], undefined); // Pass; string does not exist in input array
-assertEqual(result1["Diana"], undefined); // Pass; string does not exist in input array
