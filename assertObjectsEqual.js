9e31f17ed6f91b7b4890f7a86cbd3f5547606d65
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 02:56:47 -0400

    Remove test code because it was moved to new "assertObjectsEqualTest.js" test file.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,7 +1,6 @@
 // Require eqObjects function
 const eqObjects = require('./eqObjects');
 
-
 // assertObjectsEqual function definition
 // This function takes two objects and prints to the console whether or not they have matching contents
 const assertObjectsEqual = function(actual, expected) {
@@ -15,11 +14,3 @@ const assertObjectsEqual = function(actual, expected) {
 
 // Code to export function
 module.exports = assertObjectsEqual;
-
-// Direct call to assertObjects Equal function to confirm messages printed to the console are accurate
-const object1 = { a: '2', b: 4};
-const object2 = { b: 4, a: '2'};
-const object3 = { a: '2', b: '4'};
-
-assertObjectsEqual(object1, object2); // Should pass
-assertObjectsEqual(object1, object3); // Should fail
