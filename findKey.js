bacdb043b32180424acdd425461873aa22123ead
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 00:35:56 -0400

    Remove "assertEqual" function definition because it is not needed in this file anymore. Remove test code because it was moved to new "findKeyTest.js" test file.

--- findKey.js
+++ findKey.js
@@ -1,12 +1,4 @@
-// assertEqual function copied from previous task
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// findKey function definition
 const findKey = function(object, callback) {
   for (const key in object) {
     if (callback(object[key])) { // Check if the value of the current key matches the given value
@@ -18,18 +10,3 @@ const findKey = function(object, callback) {
 
 // Code to export function
 module.exports = findKey;
-
-// Test Code
-const restaurants = {
-  "Blue Hill": { stars: 1 },
-  Akaleri: { stars: 3 },
-  noma: { stars: 2 },
-  elBulli: { stars: 3 },
-  Ora: { stars: 2 },
-  Akelarre: { stars: 3 },
-};
-  
-assertEqual(findKey(restaurants, (x) => x.stars === 2), "noma"); // Pass
-assertEqual(findKey(restaurants, (x) => x.stars === 1), "Blue Hill"); // Pass
-assertEqual(findKey(restaurants, (x) => x.stars === 0), undefined); // Pass
-assertEqual(findKey(restaurants, (x) => x.stars === 5), undefined); // Pass
