683145aca510660e7432febd67b1789ad7c68a80
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 02:03:57 -0400

    Remove "eqArrays" and "assertArraysEqual" function definitions because they are not needed in this file anymore. Remove test code because it was moved to new "takeUntilTest.js" test file.

--- takeUntil.js
+++ takeUntil.js
@@ -1,29 +1,5 @@
-// Copied previously implemented eqArrays and assertArraysEqual functions:
-// eqArrays function checks if two arrays are equal
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// assertArraysEqual function takes in two arrays and console.log an appropriate message to the console
-// This function asserts if two arrays are equal and logs the result
-const assertArraysEqual = function(actual, expected) {
-  const isEqual = eqArrays(actual, expected);
-  if (isEqual) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// takeUntil funtion takes in an array to work with and the callback function
+// takeUntil function definition
+// funtion takes in an array to work with and the callback function
 // It returns a slice of the array with elements taken from the beginning
 const takeUntil = function(array, callback) {
   const result = [];
@@ -38,14 +14,3 @@ const takeUntil = function(array, callback) {
 
 // Code to export function
 module.exports = takeUntil;
-
-// TEST CASES
-// Test Case # 1: Slice when you encounter a number less than 0
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-
-// Test Case # 2: Slice when you encounter a comma
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
