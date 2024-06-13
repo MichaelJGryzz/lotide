1ac05dc16bd0d69f807b3d12d3ba189fd2b5fbe0
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 02:27:05 -0400

    Convert the five assertion tests to "it" statements in a "describe" callback function to serve as the new assertion tests. Delete all the old commented out test code.

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -5,29 +5,35 @@ const assert = require('chai').assert;
 const without = require('..//without');
 
 
-/*
-// TEST CASES:
-// Test Case # 1:
-const providedArray1 = [1, 2, 3];
-const result1 = without(providedArray1, [1]);
-assertArraysEqual(result1, [2, 3]);
-
-// Test Case # 2:
-const providedArray2 = ["1", "2", "3"];
-const result2 = without(providedArray2, [1, 2, "3"]);
-assertArraysEqual(result2, ["1", "2"]);
-
-// Test Case # 3:
-const providedArray3 = ["Honda", "Subaru", "Toyota", "Nissan"];
-const result3 = without(providedArray3, ["Nissan", "Mazda", "Mitsubishi"]);
-assertArraysEqual(result3, ["Honda", "Subaru", "Toyota"]);
-
-// Test Case to Ensure original array is not modified:
-const providedWords = ["hello", "world", "lighthouse"];
-const resultWords = without(providedWords, ["lighthouse"]); // No need to capture return value for this test case
-assertArraysEqual(resultWords, ["hello", "world"]);
-// Make sure the original array was not altered by the without function
-assertArraysEqual(providedWords, ["hello", "world", "lighthouse"]);
-
-// Test Case with empty source array
-assertArraysEqual(without([], [1], [2]), []);*/
+// Mocha and Chai based Test Code:
+describe("#without", () => {
+  // Test Case # 1:
+  it("returns [2, 3] for [1, 2, 3] without [1]", () => {
+    const providedArray1 = [1, 2, 3];
+    const result1 = without(providedArray1, [1]);
+    assert.deepEqual(result1, [2, 3]);
+  });
+  // Test Case # 2:
+  it("returns ['1', '2'] for ['1', '2', '3'] without [1, 2, '3']", () => {
+    const providedArray2 = ["1", "2", "3"];
+    const result2 = without(providedArray2, [1, 2, "3"]);
+    assert.deepEqual(result2, ["1", "2"]);
+  });
+  // Test Case # 3:
+  it("returns ['Honda', 'Subaru', 'Toyota'] for ['Honda', 'Subaru', 'Toyota', 'Nissan'] without ['Nissan', 'Mazda', 'Mitsubishi']", () => {
+    const providedArray3 = ["Honda", "Subaru", "Toyota", "Nissan"];
+    const result3 = without(providedArray3, ["Nissan", "Mazda", "Mitsubishi"]);
+    assert.deepEqual(result3, ["Honda", "Subaru", "Toyota"]);
+  });
+  // Test Case to Ensure original array is not modified:
+  it("does not modify the original array", () => {
+    const providedWords = ["hello", "world", "lighthouse"];
+    const resultWords = without(providedWords, ["lighthouse"]);
+    assert.deepEqual(resultWords, ["hello", "world"]);
+    assert.deepEqual(providedWords, ["hello", "world", "lighthouse"]);
+  });
+  // Test Case with empty source array
+  it("returns [] for an empty source array", () => {
+    assert.deepEqual(without([], [1], [2]), []);
+  });
+});
