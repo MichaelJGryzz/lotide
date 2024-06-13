cabaf9829bb4a3efae824ea46dd68cf0f4612b3c
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 03:02:22 -0400

    Comment out all of the old test code in preparation of adding "Mocha" and "Chai" based test code. Add require call code for "Chai" at the top of the file.

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -1,10 +1,15 @@
+// Require Chai
+const assert = require('chai').assert;
+
 //Require assertObjectsEqual function
 const assertObjectsEqual = require('../assertObjectsEqual');
 
+
+/*
 // Direct call to assertObjects Equal function to confirm messages printed to the console are accurate
 const object1 = { a: '2', b: 4};
 const object2 = { b: 4, a: '2'};
 const object3 = { a: '2', b: '4'};
 
 assertObjectsEqual(object1, object2); // Should pass
-assertObjectsEqual(object1, object3); // Should fail (on purpose!)
+assertObjectsEqual(object1, object3); // Should fail (on purpose!)*/
