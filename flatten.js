71534a26742282fb4c688b6e5bc630f146688060
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 01:15:41 -0400

    Remove "eqArrays" and "assertArraysEqual" function definitions because they are not needed in this file anymore. Remove test code because it was moved to new "flattenTest.js" tets file.

--- flatten.js
+++ flatten.js
@@ -1,27 +1,5 @@
-// eqArrays function checks if two arrays are equal
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// assertArraysEqual function asserts if two arrays are equal and logs the result
-const assertArraysEqual = function(actual, expected) {
-  const isEqual = eqArrays(actual, expected);
-  if (isEqual) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// "flatten" function takes in an array with nested arrays and returns a "flattened" version of the array without nesting
+// flatten function definition
+// function takes in an array with nested arrays and returns a "flattened" version of the array without nesting
 const flatten = function(array) {
   let result = [];
 
@@ -41,16 +19,3 @@ const flatten = function(array) {
 
 // Code to export function
 module.exports = flatten;
-
-// TEST CASES:
-// Test Case # 1:
-const nestedArray1 = [1, 2, [3, 4], 5];
-const expectedArray1 = [1, 2, 3, 4, 5];
-const flattenedArray1 = flatten(nestedArray1);
-assertArraysEqual(flattenedArray1, expectedArray1);
-
-// Test Case #2:
-const nestedArray2 = [[1, 2], [3, 4], 5, [6, 7, 8]];
-const expectedArray2 = [1, 2, 3, 4, 5, 6, 7, 8];
-const flattenedArray2 = flatten(nestedArray2);
-assertArraysEqual(flattenedArray2, expectedArray2);
