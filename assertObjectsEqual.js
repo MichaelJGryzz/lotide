45d95e7bc1ab902056444f9a94f79997bc5dd7ee
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-12 23:33:08 -0400

    Remove "eqArrays" and "eqObjects"function definitions because they are not needed in this file anymore. Add code at top of file to require "eqObjects" function.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,40 +1,6 @@
-// Copied previously implemeted eqArrays function
-// This function checks if two arrays are equal
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// Require eqObjects function
+const eqObjects = require('./eqObjects');
 
-// Copied previously implemented eqObjects function
-// This function checks if two objects are equal
-const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-  
-  // Check if both objects have same number of keys, if not return false
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-
-  for (const key of keys1) {
-
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
 
 // assertObjectsEqual function definition
 // This function takes two objects and prints to the console whether or not they have matching contents
