e48ac80c5786d8ae1be47197d6dbd7052412bccd
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 01:54:12 -0400

    Comment out all of the old test code in preparation of adding "Mocha" and "Chai" based test code. Remove require call code for "eqArrays" and "assertArraysEqual" functions because they are not needed anymore. Add require call code for "Chai" at the top of the file.

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,14 +1,12 @@
-// Require eqArrays function
-const eqArrays = require('../eqArrays');
-
-// Require assertArraysEqual function
-const assertArraysEqual = require('../assertArraysEqual');
+// Require Chai
+const assert = require('chai').assert;
 
 // Require letterPositions function
 const letterPositions = require('../letterPositions');
 
+/*
 // Test Code
 assertArraysEqual(letterPositions("hello").h, [0]);
 assertArraysEqual(letterPositions("hello").e, [1]);
 assertArraysEqual(letterPositions("hello").l, [2, 3]);
-assertArraysEqual(letterPositions("hello").o, [4]);
+assertArraysEqual(letterPositions("hello").o, [4]);*/
