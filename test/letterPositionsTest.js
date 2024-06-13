13fabfd651a963a349a06331c1a1814e710de74a
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 01:57:58 -0400

    Convert the four assertion tests to "it" statements in a "describe" function to serve as the new assertion tests. Delete all the old commented out code.

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -4,9 +4,19 @@ const assert = require('chai').assert;
 // Require letterPositions function
 const letterPositions = require('../letterPositions');
 
-/*
-// Test Code
-assertArraysEqual(letterPositions("hello").h, [0]);
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("hello").l, [2, 3]);
-assertArraysEqual(letterPositions("hello").o, [4]);*/
+
+// Mocha and Chai based Test Code:
+describe("#letterPositions", () => {
+  it("returns [0] for 'h' in 'hello'", () => {
+    assert.deepEqual(letterPositions("hello").h, [0]);
+  });
+  it("returns [1] for 'e' in 'hello'", () => {
+    assert.deepEqual(letterPositions("hello").e, [1]);
+  });
+  it("returns [2, 3] for 'l' in 'hello'", () => {
+    assert.deepEqual(letterPositions("hello").l, [2, 3]);
+  });
+  it("returns [4] for 'o' in 'hello'", () => {
+    assert.deepEqual(letterPositions("hello").o, [4]);
+  });
+});
