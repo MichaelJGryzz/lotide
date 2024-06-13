58d8624d939c9fe27030b921e9708c20686a8d47
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 02:59:59 -0400

    Create test file "assertObjectsEqualTest.js" in test folder and move test code from "assertObjectsEqual.js" file to separate it. Add code at top of file to require "assertObjectsEqual" function.

---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,10 @@
+//Require assertObjectsEqual function
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+// Direct call to assertObjects Equal function to confirm messages printed to the console are accurate
+const object1 = { a: '2', b: 4};
+const object2 = { b: 4, a: '2'};
+const object3 = { a: '2', b: '4'};
+
+assertObjectsEqual(object1, object2); // Should pass
+assertObjectsEqual(object1, object3); // Should fail (on purpose!)
