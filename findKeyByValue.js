40baf6ebe78630663848f3afaaf4a47df7c18f9e
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 00:58:00 -0400

    Remove "assertEqual" function definition because it is not needed in this file anymore. Remove test code because it was moved to new "fineKeyByValueTest.js" test file.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,12 +1,4 @@
-// assertEqual function copied from previous task
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// findKeyByValue function definition
 // Functiion finds the key in a given object by the given value
 const findKeyByValue = function(object, value) {
   for (let key of Object.keys(object)) {
@@ -19,16 +11,3 @@ const findKeyByValue = function(object, value) {
 
 // Code to export function
 module.exports = findKeyByValue;
-
-// Test Code
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire",
-  action: "Sons of Anarchy",
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); // TV Show not in test object
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Sons of Anarchy"), "action");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Peaky Blinders"), undefined); // TV Show not in test object
