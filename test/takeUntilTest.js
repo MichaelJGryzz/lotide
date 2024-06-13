a4ebf95eabeb56b06f1bf65282f39e6b298a4d40
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 02:13:08 -0400

    Convert the two assertion tests to "it" statements in a "describe" function to serve as the new assertion tests. Delete all the old commented out code.

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -5,14 +5,18 @@ const assert = require('chai').assert;
 const takeUntil = require('../takeUntil');
 
 
-/*
-// TEST CASES
-// Test Case # 1: Slice when you encounter a number less than 0
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-
-// Test Case # 2: Slice when you encounter a comma
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);*/
+// Mocha and Chai based Test Code:
+describe("#takeUntil", () => {
+  // Test Case # 1: Slice when you encounter a number less than 0
+  it("returns [ 1, 2, 5, 7, 2 ] for data1", () => {
+    const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+    const results1 = takeUntil(data1, x => x < 0);
+    assert.deepEqual(results1, [ 1, 2, 5, 7, 2 ]);
+  });
+  // Test Case # 2: Slice when you encounter a comma
+  it("returns [ 'I\'ve', 'been', 'to', 'Hollywood' ] for data2", () => {
+    const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+    const results2 = takeUntil(data2, x => x === ',');
+    assert.deepEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+  });
+});
