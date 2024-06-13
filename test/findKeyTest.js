779841ec54d0f305fb5262a8e0edad05cff4a8ff
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-13 00:51:21 -0400

    Leave "restaurants" object from previous code untouched. Convert the four assertion tests to "it" statements in a "describe" callback function to serve as the new assertion tests. Add require call code for "Chai" at the top of the file. Delete all the old commented out code.

--- test/findKeyTest.js
+++ test/findKeyTest.js
@@ -4,8 +4,8 @@ const assert = require('chai').assert;
 // Require head function
 const findKey = require('../findKey');
 
-/*
-// Test Code
+
+// Provided object of restaurants with their star ratings
 const restaurants = {
   "Blue Hill": { stars: 1 },
   Akaleri: { stars: 3 },
@@ -14,8 +14,22 @@ const restaurants = {
   Ora: { stars: 2 },
   Akelarre: { stars: 3 },
 };
-  
-assertEqual(findKey(restaurants, (x) => x.stars === 2), "noma"); // Pass
-assertEqual(findKey(restaurants, (x) => x.stars === 1), "Blue Hill"); // Pass
-assertEqual(findKey(restaurants, (x) => x.stars === 0), undefined); // Pass
-assertEqual(findKey(restaurants, (x) => x.stars === 5), undefined); // Pass*/
+
+// Mocha and Chai based Test Code:
+describe("#findKey", () => {
+  it("returns 'noma' for stars === 2", () => {
+    assert.strictEqual(findKey(restaurants, (x) => x.stars === 2), "noma");
+  });
+
+  it("returns 'Blue Hill' for stars === 1", () => {
+    assert.strictEqual(findKey(restaurants, (x) => x.stars === 1), "Blue Hill");
+  });
+
+  it("returns undefined for stars === 0", () => {
+    assert.strictEqual(findKey(restaurants, (x) => x.stars === 0), undefined);
+  });
+
+  it("returns undefined for stars === 5", () => {
+    assert.strictEqual(findKey(restaurants, (x) => x.stars === 5), undefined);
+  });
+});
