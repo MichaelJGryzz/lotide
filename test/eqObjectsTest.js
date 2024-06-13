6ff5b4b5659228ea408a6620dd85b1505a86f139
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-12 23:08:41 -0400

    Create test file "eqObjectsTest.js" in test folder and move test code from "eqObjects.js" to new test file to separate it. Add code at top of file to require "eqArrays", "assertEqual" and "eqObjects" functions.

---
+++ test/eqObjectsTest.js
@@ -0,0 +1,37 @@
+// Require eqArrays function
+const eqArrays = require('../eqArrays');
+
+// Require assertEqual function
+const assertEqual = require('../assertEqual');
+
+// Require eqObjects function
+const eqObjects = require('../eqObjects');
+
+
+// Test Code
+// Use both assertEqual and equalObjects to compare countLetters function output with expected output
+// Test Case # 1
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+eqObjects(shirtObject, anotherShirtObject); // => true
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+
+// Test Case # 2
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject, longSleeveShirtObject); // => false
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+
+// Test Case # 3
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+eqObjects(multiColorShirtObject, anotherMultiColorShirtObject); // => true
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+
+// Test Case # 4
+const longSleeveMultiColorShirtObject = {
+  size: "medium",
+  colors: ["red", "blue"],
+  sleeveLength: "long",
+};
+eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject); // => false
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
